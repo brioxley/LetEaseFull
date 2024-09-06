39a9770932bc0ad3c7eb101a6821fddf93940256
brioxley <brian.oxley@gmail.com>
2024-09-06 10:24:23 +0100

    No errors now

--- LetEase.Infrastructure/obj/Debug/net8.0/LetEase.Infrastructure.AssemblyInfo.cs
+++ LetEase.Infrastructure/obj/Debug/net8.0/LetEase.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LetEase.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73e3f4eaf4f9f0db973ce34c62a406c7a50e918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06562c2523598ea722bdfec93e7d3dd58c205f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LetEase.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LetEase.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
